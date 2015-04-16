8fe69cbe9da170dafe316653ac618d5a5ad4c7fd
Wesley Brooks <brooks.wesley@gmail.com>
2015-04-16 11:49:10 -0500

    added some debugging messages

--- R/predict.adapt.R
+++ R/predict.adapt.R
@@ -8,10 +8,8 @@ predict.adapt <-  function(obj, newx, lambda=obj$glmnet$lambda, type=c("link","r
 
 
     pred.data = newx
-print(pred.data[,obj$predcictors])
-print(class(pred.data))
+print(colnames(pred.data))
     predictors = obj$predcictors
-print(dim(pred.data[,predictors] %>% as.matrix))
     pred.data = pred.data[,predictors] %>% as.matrix %>% sweep(2, obj$meanx, '-') %>% sweep(2, obj$scale, '*')
     type = match.arg(type)
 
